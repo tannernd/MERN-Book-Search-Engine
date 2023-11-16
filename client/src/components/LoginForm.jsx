f04d81b14172492829e839e5d2d97c4ba1c271ac
Nate Tanner <40620787+tannernd@users.noreply.github.com>
2023-11-16 01:25:12 -0700

    Frontend Login and Signup forms

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -2,13 +2,15 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { LOGIN } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error, data }] = useMutation(LOGIN);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -24,17 +26,14 @@ const LoginForm = () => {
       event.preventDefault();
       event.stopPropagation();
     }
-
+    
+    console.log(userFormData);
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
+      
+      Auth.login(data.login.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
